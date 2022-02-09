ed5c18d97277f1e36344f5add20c20d8f90a3a2c
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-02-09 16:40:54 +0100

    fix: fix extensions

--- src/index.ts
+++ src/index.ts
@@ -10,31 +10,25 @@ import { assert } from './custom-assert';
 const { beTrue, beFalse, beNull, beUndefined, ...initialProperties } =
   defaultProperties;
 
-const properties: Record<string, Property[] | undefined> = {
-  ...Object.keys(initialProperties).reduce(
-    (acc, key) => ({
-      ...acc,
-      [key]: [initialProperties[key as keyof typeof initialProperties]],
-    }),
-    {} as Record<string, Property[] | undefined>
-  ),
-  satisfies: [initialProperties.satisfy],
-  equals: [initialProperties.equal],
-  includes: [initialProperties.include],
-  contain: [initialProperties.include],
-  contains: [initialProperties.include],
-  match: [initialProperties.include],
-  matches: [initialProperties.include],
-  a: [initialProperties.type],
-  an: [initialProperties.type],
-  lengthOf: [initialProperties.length],
-  instanceOf: [initialProperties.instance],
-  deeply: [initialProperties.deep],
-  function: [initialProperties.beFunction],
-  true: [beTrue],
-  false: [beFalse],
-  null: [beNull],
-  undefined: [beUndefined],
+const properties: Record<string, Property | undefined> = {
+  ...initialProperties,
+  satisfies: initialProperties.satisfy,
+  equals: initialProperties.equal,
+  includes: initialProperties.include,
+  contain: initialProperties.include,
+  contains: initialProperties.include,
+  match: initialProperties.include,
+  matches: initialProperties.include,
+  a: initialProperties.type,
+  an: initialProperties.type,
+  lengthOf: initialProperties.length,
+  instanceOf: initialProperties.instance,
+  deeply: initialProperties.deep,
+  function: initialProperties.beFunction,
+  true: beTrue,
+  false: beFalse,
+  null: beNull,
+  undefined: beUndefined,
 };
 
 export function expect(value: any) {
@@ -55,28 +49,16 @@ export function expect(value: any) {
   chainContext.clearFlags = clearFlags.bind(chainContext);
   chainContext.assert = assert.bind(chainContext);
   internalFlags.set('object', value);
-  const boundProps = Object.keys(properties).reduce((acc, key) => {
-    if (key === 'default') return acc;
-    return {
-      ...acc,
-      [key]: properties[key]?.map((p) => {
-        return {
-          onAccess: p.onAccess?.bind(chainContext),
-          onCall: p.onCall?.bind(chainContext),
-        };
-      }),
-    };
-  }, {} as typeof properties);
-  const proxy: any = new Proxy(boundProps, {
+  const proxy: any = new Proxy(properties, {
     get(target, prop) {
       if (typeof prop !== 'string') return;
-      const props = target[prop];
-      if (!props || props.length === 0) return proxy;
-      props.forEach((p) => p.onAccess?.());
-      if (props.some((p) => p.onCall)) {
+      const property = target[prop];
+      if (!property?.onAccess && !property?.onCall) return proxy;
+      property.onAccess?.call(chainContext);
+      if (property.onCall) {
         return new Proxy(
           function (...args: unknown[]) {
-            props.forEach((p) => p.onCall?.(...args));
+            property.onCall?.apply(chainContext, args);
             return proxy;
           },
           {
@@ -95,8 +77,17 @@ export function expect(value: any) {
 function addProperty(names: string | string[], handler: Property): void {
   names = Array.isArray(names) ? names : [names];
   for (const name of names) {
-    if (!properties[name]) properties[name] = [handler];
-    else properties[name]!.push(handler);
+    if (!properties[name]) properties[name] = handler;
+    else {
+      properties[name] = {
+        onAccess(this: Context) {
+          handler.onAccess?.call(this);
+        },
+        onCall(this: Context, ...args: any[]) {
+          handler.onCall?.apply(this, args);
+        },
+      };
+    }
   }
 }
 
@@ -106,16 +97,8 @@ function replaceProperty(
 ) {
   names = Array.isArray(names) ? names : [names];
   for (const name of names) {
-    const property = properties[name] ?? [];
-    const handler: Property = {
-      onCall(this: Context, ...args: any[]) {
-        property.forEach((p) => p.onCall?.(...args));
-      },
-      onAccess(this: Context) {
-        property.forEach((p) => p.onAccess?.());
-      },
-    };
-    properties[name] = [{ ...handler, ...extender(handler) }];
+    const handler = properties[name] ?? {};
+    properties[name] = { ...handler, ...extender(handler) };
   }
 }
 
